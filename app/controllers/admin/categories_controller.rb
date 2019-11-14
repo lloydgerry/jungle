4b4cbfd9fbe6547821696105c06ec6c09fc3c9bc
Lloyd <lloyd.gerry@gmail.com>
2019-11-14 22:31:37 +0000

    feature that will add new categories to the db

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,7 +13,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if  @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
@@ -22,12 +22,17 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
   private
 
   def category_params
     params.require(:category).permit(
       :name,
-      :category_id,
     )
   end
 
